a8e0011d286ec7a2b975503547680e775ece785f
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-08-05 13:39:28 -0400

    Made some design changes

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,8 +3,9 @@ class Phone < ApplicationRecord
   belongs_to :user
   # self.primary_key = 'user_id'
 
-   validates :phone_number, :presence => {:message => 'can not be blank'},
-                     :numericality => true,
+  validates :phone_number,:allow_blank => true,
+                      :presence => {:message => 'can not be blank'},
+                     :numericality => true, 
                      :length => { :minimum => 10, :maximum => 13 }
   # set_as_primary :primary, owner_key: :user
 end
