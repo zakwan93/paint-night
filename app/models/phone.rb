1bd5a6def894a13377855a174b2801f8f311ab48
zakwan93 <zakwan_bhaiyat@yahoo.com>
2021-08-05 12:35:10 -0400

    Designed the error message for validation for registration form

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,7 +3,7 @@ class Phone < ApplicationRecord
   belongs_to :user
   # self.primary_key = 'user_id'
 
-   validates :phone_number, :presence => {:message => 'is not valid'},
+   validates :phone_number, :presence => {:message => 'can not be blank'},
                      :numericality => true,
                      :length => { :minimum => 10, :maximum => 13 }
   # set_as_primary :primary, owner_key: :user
